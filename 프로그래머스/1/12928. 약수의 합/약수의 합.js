c129af01ccaa840dc2199bdbf570a5a1878d60e8
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-01-26 16:38:28 +0900

    [level 1] Title: 약수의 합, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,8 +1,8 @@
 function solution(n) {
-  let numbers = [];
+  let numbers = 0;
   const array = new Array(n + 1).fill(0);
   array.forEach((_el, idx) => {
-    if (n % idx === 0) numbers.push(idx);
+    if (n % idx === 0) numbers += idx;
   });
-  return numbers.reduce((acc, v) => acc + v, 0);
+  return numbers;
 }
