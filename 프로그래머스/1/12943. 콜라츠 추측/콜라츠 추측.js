5796d67909a97ea8aad34df9e020888479fb4048
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-01-29 16:39:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+function solution(num,answer=0) {
+    while (num != 1 && answer !=500) {
+    num % 2 == 0 ? num /= 2: num = (num * 3) + 1;
+    answer++;
+    }
+    return num == 1 ? answer : -1
+    
+}
+
