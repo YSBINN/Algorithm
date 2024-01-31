2b09ce20e620698d5a666e55dcb707be498988d0
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-01-31 16:43:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.46 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,8 +1,3 @@
 function solution(arr) {
-  return arr.reduce((prev, cur, i) => {
-    if (i === 0 || cur !== arr[i - 1]) {
-      prev.push(cur);
-    }
-    return prev;
-  }, []);
+  return arr.filter((el, i) => el !== arr[i + 1]);
 }
