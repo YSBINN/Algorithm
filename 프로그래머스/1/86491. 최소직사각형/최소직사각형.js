54ed128efaed0192618673c53d0c78e36d5939c6
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-02-02 19:06:38 +0900

    [level 1] Title: 최소직사각형, Time: 9.04 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,6 +1,5 @@
 function solution(sizes) {
 const sortedSizes = sizes.map(el=>el.sort((a,b)=>a-b))
-const width = Math.max(...sortedSizes.map(el=>el[0]))
-const height =Math.max(...sortedSizes.map(el=>el[1]))
+const [width,height] = sortedSizes.reduce(([maxW,maxH],[curW,curH])=>[Math.max(maxW,curW),Math.max(maxH,curH)],[0,0])
 return width*height
 }
