964f3228a8f1f3705eb630ce168ce2c051eae47d
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-02-08 16:30:39 +0900

    [level 1] Title: 추억 점수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,9 +1,3 @@
-function solution(n, y, p) {
-    let m = new Map()
-    n.forEach((el,i)=>m.set(el,y[i]))
-    const answer = p.map(el=>el.map(e=>m.get(e)).reduce((acc,cur)=>{
-        if(!cur)cur=0
-        return acc+cur
-    },0))
-    return answer
+function solution(name, yearning, photo) {
+    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0))
 }
