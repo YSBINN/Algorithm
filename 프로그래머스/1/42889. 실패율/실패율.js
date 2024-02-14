ba5181ca3fd1e99a895f24e1c81d493533354099
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-02-14 18:59:06 +0900

    [level 1] Title: 실패율, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,7 @@
+function solution(N, stages) {
+    return Array.from({ length: N }, (_, i) => 
+    { const stage = i+1;
+      return { stage, rate:stages.filter(s => s === stage).length / stages.filter(s => s >= stage).length}}) 
+        .sort((a, b) => b.rate - a.rate) 
+        .map(({ stage }) => stage); 
+}
