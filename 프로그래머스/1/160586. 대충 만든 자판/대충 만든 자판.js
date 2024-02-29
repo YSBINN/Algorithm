700cf6ae4e871376889153d907e6f4655a739096
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-02-29 11:27:14 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.02 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,27 +1,22 @@
+
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
 function solution(keymap, targets) {
-  const map = new Map();
-  const answer = [];
-
-  keymap.forEach(keymapNum => {
-    for (let j = 0; j < keymapNum.length; j++) {
-      if (!map.has(keymapNum[j]) || j < map.get(keymapNum[j])) {
-        map.set(keymapNum[j], j + 1);
-      }
+    const answer = [];
+    const map = {}
+    for (const items of keymap) {
+        items.split('').map((item, index) => map[item] = (map[item] < index+1 ? map[item] : index+1))
     }
-  });
-
-  targets.forEach(target => {
-    let result = 0;
-    for (let char of target) {
-      if (!map.has(char)) {
-        result = -1;
-        break;
-      } else {
-        result += map.get(char);
-      }
+    for (const items of targets) {
+        answer.push(items.split('').reduce((cur, item) => cur += map[item], 0) || -1)
     }
-    answer.push(result);
-  });
-
-  return answer;
+    return answer;
 }
