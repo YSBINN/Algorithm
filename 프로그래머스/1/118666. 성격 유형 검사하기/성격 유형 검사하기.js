62ea9d900402440b6e1f3294e943d62cfe6fa584
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-03-08 18:47:44 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.49 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,18 +1,16 @@
 function solution(survey, choices) {
-  const types = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
-  let answer = "";
-  for (let i = 0; i < choices.length; i++) {
-    const [leftType, rightType] = survey[i].split("");
-    const result = Math.abs(choices[i] - 4);
-    if (choices[i] < 4) types[leftType] += result;
-    if (choices[i] > 4) types[rightType] += result;
-  }
-  const type = Object.keys(types);
-  for (let i = 0; i < type.length; i += 2) {
-    const leftType = types[type[i]];
-    const rightType = types[type[i + 1]];
-    leftType >= rightType ? (answer += type[i]) : (answer += type[i + 1]);
-  }
+    const MBTI = {};
+    const types = ["RT","CF","JM","AN"];
 
-  return answer;
+    types.forEach((type) =>
+        type.split('').forEach((char) => MBTI[char] = 0)
+    )
+
+    choices.forEach((choice, index) => {
+        const [disagree, agree] = survey[index];
+
+        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
+    });
+
+    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
 }
