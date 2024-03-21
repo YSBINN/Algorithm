aa3e55a24dbd190cc0276efdc1e368dcc7a1881c
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-03-21 14:36:07 +0900

    [level 2] Title: 피보나치 수, Time: 5.06 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,17 +1,7 @@
 function solution(n) {
-  let answer = 0;
-
-  let dp = new Array(n + 1);
-
-  dp[0] = 0;
-  dp[1] = 1;
-  dp[2] = 1;
-
+  const dp = [0, 1, 1];
   for (let i = 3; i <= n; i++) {
     dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
   }
-
-  answer = dp[n];
-
-  return answer;
+  return dp[n];
 }
