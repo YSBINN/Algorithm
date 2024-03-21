32074ad7771a4e1748c1e801fb05f3cc33866c65
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-03-21 17:15:55 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842#qna) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 21일 17:05:33
+2024년 03월 21일 17:15:54
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,13 +1,10 @@
 function solution(brown, yellow) {
-  const total = brown + yellow;
-  const divisors = new Map();
-  for (let i = 3; i <= Math.sqrt(total); i++) {
-    if (total % i === 0) {
-      divisors.set(total / i, i);
+  for (let height = 3; height <= (brown + yellow) / height; height++) {
+    if ((brown + yellow) % height === 0) {
+      let width = (brown + yellow) / height;
+      if ((width - 2) * (height - 2) === yellow) {
+        return [width, height];
+      }
     }
   }
-  return Array.from(divisors).filter(el => {
-    const [width, height] = el;
-    return (width - 2) * (height - 2) === yellow;
-  })[0];
 }
