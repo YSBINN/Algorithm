37c3f48264e671b2a4da1d04dad957422db7e8c6
YSBINN <105031831+YSBINN@users.noreply.github.com>
2024-03-21 15:28:02 +0900

    [level 2] Title: 짝지어 제거하기, Time: 39.33 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+  const stack = [];
+  for (const char of s) {
+    if (stack[stack.length - 1] === char) stack.pop();
+    else stack.push(char);
+  }
+  return stack.length ? 0 : 1;
+}
